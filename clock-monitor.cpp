27e6dcfdb278f62241f6023744c88b7722dd6e9b
Xuefer <xuefer@gmail.com>
2016-11-20 15:23:26 +0800

    fix output of nanosecs part using double
    Signed-off-by: Xuefer <xuefer@gmail.com>

--- clock-monitor.cpp
+++ clock-monitor.cpp
@@ -5,51 +5,17 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <iomanip>
 
-timespec clock(clockid_t clk_id)
+enum {
+	NANO_PER_SECOND = 1000000000
+};
+
+double clock(clockid_t clk_id)
 {
 	timespec tv;
 	clock_gettime(clk_id, &tv);
-	return tv;
-}
-
-timespec operator -(const timespec &time1, const timespec &time2)
-{
-	timespec result;
-
-	/* Subtract the second time from the first. */
-	/* TIME1 < TIME2? */
-	if (time1.tv_sec - time2.tv_sec < 0 ||
-		(time1.tv_sec == time2.tv_sec && time1.tv_nsec - time2.tv_nsec < 0)) {
-		result = time2 - time1;
-		result.tv_sec = -result.tv_sec;
-		result.tv_nsec = -result.tv_nsec;
-	} else {
-		result.tv_sec = time1.tv_sec - time2.tv_sec;
-		if (time1.tv_nsec - time2.tv_nsec < 0) {
-			result.tv_nsec = time1.tv_nsec + 1000000000L - time2.tv_nsec;
-			result.tv_sec--;
-		} else {
-			result.tv_nsec = time1.tv_nsec - time2.tv_nsec;
-		}
-	}
-
-	return result;
-}
-
-std::ostream &operator <<(std::ostream &os, const timespec &tv) {
-	std::ostringstream str;
-	if (tv.tv_nsec < 0 && tv.tv_sec == 0) {
-		str << '-';
-	} else {
-		str << '+';
-	}
-	str << tv.tv_sec << '.' << abs(tv.tv_nsec);
-
-	os << str.str();
-	return os;
+	return tv.tv_sec + static_cast<double>(tv.tv_nsec) / NANO_PER_SECOND;
 }
 
 std::string readLine(const char *path) {
@@ -58,39 +24,61 @@ std::string readLine(const char *path) {
 	return s;
 }
 
+class comma_numpunct : public std::numpunct<char>
+{
+protected:
+	virtual char do_thousands_sep() const
+	{
+		return ',';
+	}
+
+	virtual std::string do_grouping() const
+	{
+		return "\03";
+	}
+};
+
 int main() {
-	timespec started = clock(CLOCK_REALTIME);
-	timespec oldDiffMonotonic = started - clock(CLOCK_MONOTONIC);
-	timespec oldDiffBoottime  = started - clock(CLOCK_BOOTTIME);
-
+	using std::cout;
 	using std::setw;
-	for (;;) {
-		timespec now = clock(CLOCK_REALTIME);
-		timespec newDiffMonotonic = now - clock(CLOCK_MONOTONIC);
-		timespec newDiffBoottime  = now - clock(CLOCK_BOOTTIME);
+	using std::right;
+	using std::left;
+	using std::endl;
 
-		std::cout
-			<< std::right
+	cout.imbue(std::locale(std::locale(), new comma_numpunct()));
+	cout << std::fixed;
+
+	auto started = clock(CLOCK_REALTIME);
+	auto oldDiffMonotonic = started - clock(CLOCK_MONOTONIC);
+	auto oldDiffBoottime  = started - clock(CLOCK_BOOTTIME);
+
+	for (;;) {
+		auto now = clock(CLOCK_REALTIME);
+		auto newDiffMonotonic = now - clock(CLOCK_MONOTONIC);
+		auto newDiffBoottime  = now - clock(CLOCK_BOOTTIME);
+
+		cout
+			<< right
 			<< setw(13) << now - started
-			<< std::left
+			<< left
 			<< " freq="
 			<< setw(12) << readLine("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq")
 			<< "|"
 			<< readLine("/sys/kernel/cluster/active")
-			<< std::endl;
-		std::cout
-			<< std::left
+			<< endl;
+		cout
+			<< left
 			<< "Mnew:"    << setw(22) << newDiffMonotonic
 			<< " - Mold:" << setw(22) << oldDiffMonotonic
 			<< " = "      << setw(12) << newDiffMonotonic - oldDiffMonotonic
-			<< std::endl;
+			<< endl;
 
-		std::cout
-			<< std::left
+		cout
+			<< left
 			<< "Bnew:"    << setw(22) << newDiffBoottime
 			<< " - Bold:" << setw(22) << oldDiffBoottime
 			<< " = "      << setw(12) << newDiffBoottime - oldDiffBoottime
-			<< std::endl;
+			<< endl;
 
 		sleep(10);
 	}
